ec894544ca792f129636f8c5d7786b8316aca8ea
mest2011 <mest2011mest@gmail.com>
2020-08-20 16:42:37 -0300

    First commit - Implements sum in calculator Api

---
+++ RestWithASPNET/RestWithASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : Controller
+    {
+        //GET api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = CovertToDecimal(firstNumber) + CovertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private decimal CovertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
